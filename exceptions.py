f50d35294846432987066a09d56fdcf2fbecd9f2
허예림 <rimi@appleui-MacBook-Air-2.local>
2017-07-20 23:37:42 +0900

    chapter3 예외처리 try, except, finally 완료

---
+++ exceptions.py
@@ -0,0 +1,19 @@
+import os
+
+os.chdir('../chapter3')
+a = os.getcwd()
+print(a)
+
+data = open('sketch.txt')
+
+for each_line in data:
+    try:
+        (role, line_spoken) = each_line.split(':', 1)
+        print(role, end='')
+        print(' said:', end='')
+        print(line_spoken, end='')
+    except:
+        pass
+
+
+data.close()
